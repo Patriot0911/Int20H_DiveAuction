072a5e8eb58334b266d4f1e3d93d4731af2f9e8c
Halai Andrii <halaiandriy@gmail.com>
2024-02-07 18:03:20 +0200

    Config module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,18 @@
 import { APP_PIPE } from '@nestjs/core';
 import { Module, ValidationPipe } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { DbModule } from './modules/db/db.module';
 
 @Module({
-  imports: [DbModule],
+  imports: [
+    DbModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      ignoreEnvFile: process.env.NODE_ENV === 'production',
+      load: [],
+    }),
+  ],
   controllers: [],
   providers: [
     {
