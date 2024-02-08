cf5d9d52da88a7265dd0f3453f5bf7cb0115fcb2
#Patriot0911 <75264092+Patriot0911@users.noreply.github.com>
2024-02-08 18:40:06 +0200

    div bug

---
+++ auction_frontend/src/components/pageContents/MainContent/MainContent.tsx
@@ -0,0 +1,45 @@
+import './MainContent.css';
+
+const MainContent = () => {
+    return (
+        <div
+            className={'main-page-container'}
+        >
+            <p>ss</p>
+            <p>ss</p>
+            <p>ss</p>
+            <p>ss</p>
+            <p>ss</p>
+            <p>ss</p>
+            <p>ss</p>
+            <p>ss</p>
+            <p>ss</p>
+            <p>ss</p>
+            <p>ss</p>
+            <p>ss</p>
+            <p>ss</p>
+            <p>ss</p>
+            <p>ss</p>
+            <p>ss</p>
+            <p>ss</p>
+            <p>ss</p>
+            <p>ss</p>
+            <p>ss</p>
+            <p>ss</p>
+            <p>ss</p>
+            <p>ss</p>
+            <p>ss</p>
+            <p>ss</p>
+            <p>ss</p>
+            <p>ss</p>
+            <p>ss</p>
+            <p>ss</p>
+            <p>ss</p>
+            <p>ss</p>
+            <p>ss</p>
+            <p>ss</p>
+        </div>
+    );
+};
+
+export default MainContent;
