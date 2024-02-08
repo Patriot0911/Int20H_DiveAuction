2444505ffb3b00d3dfe8c304ea23192bf2c26a10
Ivan Pasternak <retriumpf@gmail.com>
2024-02-08 20:15:48 +0200

    Improve multipart

--- backend/src/common/interceptors/upload.interceptor.ts
+++ backend/src/common/interceptors/upload.interceptor.ts
@@ -9,7 +9,8 @@ import {
   BadRequestException,
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
-import type { MultipartFile, MultipartFields } from '@fastify/multipart';
+import type { MultipartFile } from '@fastify/multipart';
+import type { FastifyRequest } from 'fastify';
 
 interface Options {
   dir: string;
@@ -20,9 +21,11 @@ interface Options {
   mkdir?: boolean;
 }
 
+const NOOP = () => {};
 const UPLOAD_DIR = join(process.cwd(), '/public/uploaded');
 const UPLOAD_TIMEOUT = 5000;
 const UPLOADED_FIELD = 'uploaded';
+const MAX_FILENAME_LENGTH = 50;
 
 export class UploadInterceptor implements NestInterceptor {
   private readonly dirPath: string;
@@ -30,7 +33,6 @@ export class UploadInterceptor implements NestInterceptor {
   private readonly maxUploadedFiles: number;
   private readonly uploadTimeout: number;
   private readonly uploadedField: string;
-  private readonly errors: Set<string>;
   private index: number = 0;
   private files: string[];
 
@@ -48,27 +50,60 @@ export class UploadInterceptor implements NestInterceptor {
     this.uploadTimeout = options.uploadTimeout ?? UPLOAD_TIMEOUT;
     this.uploadedField = options.uploadedField ?? UPLOADED_FIELD;
     this.dirPath = dirPath;
-    this.errors = new Set();
     this.files = [];
   }
 
+  normalizeFilename(filename: string): string {
+    const length = Math.min(filename.lastIndexOf('.'), MAX_FILENAME_LENGTH);
+    const normalized = filename
+      .slice(0, length)
+      .replace(/[^a-z0-9.]/gi, '_')
+      .toLowerCase();
+    return `${this.index++}-${normalized}${extname(filename)}`;
+  }
+
   async upload(part: MultipartFile): Promise<void> {
-    if (!this.mimeTypes.includes(part.mimetype))
-      return void this.errors.add(
-        `Invalid file type ${part.mimetype} of ${part.filename}`,
+    const { mimetype, filename } = part;
+    if (!this.mimeTypes.includes(mimetype))
+      throw new BadRequestException(
+        `Invalid file type ${mimetype} of ${filename}`,
       );
-    const ext = extname(part.filename);
-    const filePath = join(this.dirPath, `${this.index++}${ext}`);
+    const normalized = this.normalizeFilename(filename);
+    const filePath = join(this.dirPath, normalized);
     try {
       await pipeline(part.file, fs.createWriteStream(filePath), {
         signal: AbortSignal.timeout(this.uploadTimeout ?? UPLOAD_TIMEOUT),
       });
       this.files.push(filePath);
     } catch (err) {
-      if (err.name === 'AbortError')
-        this.errors.add(`Exceeded upload timeout of ${part.filename}`);
-      else this.errors.add(`Failed to upload ${part.filename}`);
+      throw new BadRequestException(
+        err.name === 'AbortError'
+          ? `Exceeded upload timeout of ${filename}`
+          : `Failed to upload ${filename}`,
+      );
     }
+  }
+
+  async process(req: FastifyRequest) {
+    req.body = {} as { [key: string]: unknown };
+    const parts = req.parts();
+    let uploadedFiles = 0;
+    for await (const part of parts) {
+      if (part.type === 'file') {
+        if (++uploadedFiles > this.maxUploadedFiles)
+          throw new BadRequestException(`Exceeded max uploaded files limit`);
+        else await this.upload(part);
+        continue;
+      }
+      try {
+        req.body[part.fieldname] = JSON.parse(part.value as any);
+      } catch (err) {
+        throw new BadRequestException(
+          `Invalid format of ${part.fieldname}. Expected JSON`,
+        );
+      }
+    }
+    req.body[this.uploadedField] = this.files;
   }
 
   async intercept(
@@ -76,32 +111,19 @@ export class UploadInterceptor implements NestInterceptor {
     handler: CallHandler<any>,
   ): Promise<Observable<any>> {
     const req = ctx.switchToHttp().getRequest();
-    const parts: (MultipartFile & MultipartFields)[] = req.parts();
-    req.body = {} as { [key: string]: unknown };
-    let uploadedFiles = 0;
-    for await (const part of parts) {
-      if (part.file) {
-        if (++uploadedFiles > this.maxUploadedFiles)
-          this.errors.add(`Exceeded max uploaded files limit`);
-        else await this.upload(part);
-        continue;
-      }
-      try {
-        req.body[part.fieldname] = JSON.parse(part.value as any);
-      } catch (err) {
-        this.errors.add(`Invalid format of ${part.fieldname}. Expected JSON`);
-      }
-    }
-    req.body[this.uploadedField] = this.files;
-    this.files = [];
-    if (this.errors.size) {
-      const error = new BadRequestException([...this.errors].join('\n'));
-      this.errors.clear();
-      throw error;
+    try {
+      await this.process(req);
+      this.files = [];
+    } catch (err) {
+      Promise.all(this.files.map((file) => fs.promises.unlink(file))).catch(
+        NOOP,
+      );
+      this.files = [];
+      throw err;
     }
     return handler.handle();
   }
 }
 
-export const Upload = (options?: Options) =>
+export const Upload = (options: Options) =>
   UseInterceptors(new UploadInterceptor(options));
