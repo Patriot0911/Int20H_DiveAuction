55db98c01799404996eb0a9fc87efc106ea1081a
notnuff <yaroshsasha960@gmail.com>
2024-02-10 05:01:58 +0200

    Create auction is done

---
+++ auction_frontend/src/app/create-auction/page.tsx
@@ -0,0 +1,22 @@
+import { Metadata } from "next";
+import CreateAuction from "@/components/CreateAuction/CreateAuction";
+import BackgroundDecals from "@/components/BackgroundDecals/BackgroundDecals";
+import signupList from "@/decalLists/signup";
+
+export const metadata: Metadata = {
+    title: 'Auction Dive | My Lots'
+};
+
+const MyAuctionsPage = () => {
+    return (
+        <>
+            <BackgroundDecals
+                {...signupList}
+            />
+            <CreateAuction/>
+
+        </>
+    );
+};
+
+export default MyAuctionsPage;
